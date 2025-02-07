74490e25f0e6ac62ca3cbb372497370172bb89be
ARianHidayat <ahmadrh31@gmail.com>
2025-02-07 15:07:17 +0700

    menambahkan console ke dua

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ console.log("coba untuk git")
 // kemudian saya disini menambahkan komentar untuk menambahkan keterangan
 // dan ini yang kedua
 
-let pertama = "ini percobaan"
+console.log("konsol kedua")
