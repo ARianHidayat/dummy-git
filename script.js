d99f3857f87cf43f372c10e09d595d559795dff2
ARianHidayat <ahmadrh31@gmail.com>
2025-02-07 15:59:12 +0700

    saya menambahkan variabel dan konsole log

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("coba untuk git")
 // kemudian saya disini menambahkan komentar untuk menambahkan keterangan
 // dan ini yang kedua
-
-console.log("konsol kedua")
+let tambah = "ini untuk menyoba konflik"
+console.log("konsol kedua, yang diedit dikit") 
+console.log(tambah)
